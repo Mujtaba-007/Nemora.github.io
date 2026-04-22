05a645ac43a9059a83592238dd0a7195c3baa389
Ameenullahh <ameenullahhhyderr@gmail.com>
2026-04-22 16:02:06 +0530

    Configure GitHub Pages for subfolder

--- nemora-frontend-main/next.config.mjs
+++ nemora-frontend-main/next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', 
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  // This matches your repo name so CSS/JS loads correctly
+  basePath: '/Nemora.github.io', 
 }
 
 export default nextConfig
